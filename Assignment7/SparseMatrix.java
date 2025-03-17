6795301172d617047a4027d50551c6702d9886e5
Kanishk Pamecha <kanishk.pamecha@metacube.com>
2025-03-17 17:23:52 +0530

    Assignment 7

--- Assignment7/SparseMatrix.java
+++ Assignment7/SparseMatrix.java
@@ -1,19 +1,30 @@
-import java.util.HashMap;
-
+import java.util.*;
 public class SparseMatrix {
 
     boolean square = true;
     int row = 0;
     int column = 0;
-   final HashMap<Pos, Integer> mp = new HashMap<>();
+    // comporator compares the value in the hashmap and keeps in the sorted way
+    private Comparator<Pos> sorter = (a, b) -> {
+        if(a.i == b.i){
+            return a.j - b.j;
+        }
+        return a.i - b.i;
+    };
+   final Map<Pos, Integer> mp = new TreeMap<>(sorter);
 
+   //constructor
     SparseMatrix(int[][] array) {
         row = array.length;
         column = array[0].length;
+        //checking if it square or not 
         if (row != column)
             square = false;
+            //looping throught the loop for row
         for (int i = 0; i < row; i++) {
+            // looping for the column 
             for (int j = 0; j < column; j++) {
+                // if not equal to zero add it in the hashmap
                 if (array[i][j] != 0) {
                     Pos p = new Pos(i, j);
                     mp.put(p, array[i][j]);
@@ -24,11 +35,12 @@ public class SparseMatrix {
     }
 
     /**
-     * transPose 
-     * @return HashMap<Pos, Integer>
+     * transPose transposes the matrix gives the alue at i,j to j,i
+     * @return HashMap<Pos, Integer> of the transposed matrix
      */
-    protected HashMap<Pos, Integer> transPose() {
-        HashMap<Pos, Integer> newtransPose = new HashMap<>();
+    protected Map<Pos, Integer> transPose() {
+        Map<Pos, Integer> newtransPose = new TreeMap<>(sorter);
+        // iterate all the non zero values of the matrix and gives it to new hashmap in tranposed pos.
         for (var position : mp.keySet()) {
             Pos p = new Pos(position.j, position.i);
             newtransPose.put(p, mp.get(position));
@@ -37,11 +49,18 @@ public class SparseMatrix {
         return newtransPose;
     }
 
+    /**
+     * symmetrical checks the symmetrical or not
+     * @return boolean if symmetric return true else false
+     */
     protected boolean symmetrical() {
+        // if it is not square matrix return false
         if (!square)
             return false;
+        // iterate all the non zero values of the matrix and cheks it to symmetrical position if it is not same return false.
         for (var p : mp.keySet()) {
 
+            //if not same return false
             if (mp.get(new Pos(p.j, p.i)) != mp.get(p)) {
                 System.out.println(mp.get(new Pos(p.j, p.i)) + "" + mp.get(p));
                 return false;
@@ -51,18 +70,25 @@ public class SparseMatrix {
 
     }
 
-    protected HashMap<Pos, Integer> addition(SparseMatrix sm) {
+    /**
+     * addition performs addition of two sparse matrix 
+     * @param  SparseMatrix sm object of the SparseMatrix class to be added with th current object
+     * @return HashMap<Pos,Integer> which stores the result of the addition of two matrix 
+     */
+    protected Map<Pos, Integer> addition(SparseMatrix sm) {
 
-        HashMap<Pos, Integer> ans = new HashMap<>();
-
+        Map<Pos, Integer> ans = new TreeMap<>(sorter);
+        // iterating for the 1st object and adding in the answer
         for (var p : mp.keySet()) {
             ans.put(p, mp.get(p));
-
         }
+        // iterating over the 2nd object hashmap and adding in the answer 
         for (var p : sm.mp.keySet()) {
+            // checking there is already present for this key 
             if (ans.containsKey(p)) {
                 ans.put(p, ans.get(p) + sm.mp.get(p));
-            } else {
+            } 
+            else {
                 ans.put(p, sm.mp.get(p));
             }
 
@@ -71,32 +97,29 @@ public class SparseMatrix {
         return ans;
     }
 
-    HashMap<Integer, Integer> findrows(int a, HashMap<Pos, Integer> map) {
-        HashMap<Integer, Integer> ans = new HashMap<>();
-        for (var clas : map.keySet()) {
-            if (clas.i == a) {
-                ans.put(clas.j, map.get(clas));
-            }
-        }
-        return ans;
-    }
+    /**
+     * 
+     * @param a
+     * @param map
+     * @return
+     */
+  
 
-    HashMap<Integer, Integer> findcolums(int a, HashMap<Pos, Integer> map) {
-        HashMap<Integer, Integer> ans = new HashMap<>();
-        for (var clas : map.keySet()) {
-            if (clas.j == a) {
-                ans.put(clas.j, map.get(clas));
-            }
-        }
-        return ans;
-    }
 
-    protected HashMap<Pos, Integer> multiplication(SparseMatrix sm) {
-        HashMap<Pos, Integer> ans = new HashMap<>();
+    /**
+     * multiplication multiplies the current object map and passed object map
+     * @param SparseMatrix sm  object of the 
+     * @return
+     */
+    protected Map<Pos, Integer> multiplication(SparseMatrix sm) {
+        Map<Pos, Integer> ans = new TreeMap<>(sorter);
+        // iterating the outer loop for the colums
         for (int i = 0; i < column; i++) {
+            // iterating for the inner loop for the rows of sm object 
             for (int j = 0; j < sm.row; j++) {
+                // iterating through the k common 
                 for (int k = 0; k < row; k++) {
-                    
+                       // putting it in the hashmap 
                         ans.put(new Pos(i, j),
                                 (mp.getOrDefault(new Pos(i, k), 0) * sm.mp.getOrDefault(new Pos(k, j), 0))+ans.getOrDefault(new Pos(i, j),0));
                     
@@ -107,14 +130,10 @@ public class SparseMatrix {
         return ans;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((mp == null) ? 0 : mp.hashCode());
-        return result;
-    }
-
+   
+    /**
+     * Checking it the Map are equal or not 
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -131,6 +150,8 @@ public class SparseMatrix {
             return false;
         return true;
     }
+
+   
 
 
 
--- Assignment7/SparseTest.java
+++ Assignment7/SparseTest.java
@@ -1,53 +1,127 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.stream.Stream;
-
-import javax.swing.text.html.HTMLDocument.HTMLReader.SpecialAction;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class SparseTest {
     public static void main(String[] args) {
-        SparseMatrix sm = new SparseMatrix(new int[][]{{3, 7},{4, 9}});
-        SparseMatrix sm1 = new SparseMatrix(new int[][]{{6, 2},{5, 8}});
-        HashMap<Pos,Integer> ans = sm.multiplication(sm1);
+        SparseMatrix sm = new SparseMatrix(new int[][] { { 3, 7 }, { 4, 9 } });
+        SparseMatrix sm1 = new SparseMatrix(new int[][] { { 6, 2 }, { 5, 8 } });
+        Map<Pos, Integer> ans = sm.addition(sm1);
         for (var i : ans.keySet()) {
-            System.out.println(i.i + " " +i.j + " " +ans.get(i));
-            
+            System.out.println(i.i + " " + i.j + " " + ans.get(i));
+
+        }
+        System.out.println("correct ans");
+        SparseMatrix result =  new SparseMatrix(new int[][] { { 53, 62 }, { 69, 80 } });
+        for (var i : result.mp.keySet()) {
+            System.out.println(i.i + " " + i.j + " " + result.mp.get(i));
+
         }
     }
-    
-    
-        private static Stream<Arguments> multiply(){
-            return Stream.of(
-                    Arguments.of(new SparseMatrix(new int[][]{{3, 7},{4, 9}}),
-                            new SparseMatrix(new int[][]{{6, 2}, {5, 8}}),
-                            new SparseMatrix(new int[][]{{53, 62}, {69, 80}})) );
-        }
-    
-        @ParameterizedTest
-        @MethodSource("multiply")
-        void testMultiply(SparseMatrix a , SparseMatrix b, SparseMatrix result) throws Exception{
-            assertFalse(a.multiplication(b).equals(result));
-            
-        }
-        private static Stream<Arguments> addition(){
-            return Stream.of(
-                    Arguments.of(new SparseMatrix(new int[][]{{3, 7},{4, 9}}),
-                            new SparseMatrix(new int[][]{{6, 2}, {5, 8}}),
-                            new SparseMatrix(new int[][]{{7, 9}, {9, 17}})) );
-        }
-    
-        @ParameterizedTest
-        @MethodSource("addition")
-        void testadditio(SparseMatrix a , SparseMatrix b, SparseMatrix result) throws Exception{
-            assertTrue(a.multiplication(b).equals(result));
-            
-        }
 
+    /**
+     * multiply returns and stream of Arguments to be checked as a input
+     * 
+     * @return Stream<Arguments> or the input to be checked
+     */
+    private static Stream<Arguments> multiply() {
+        return Stream.of(
+                Arguments.of(new SparseMatrix(new int[][] { { 3, 7 }, { 4, 9 } }),
+                        new SparseMatrix(new int[][] { { 6, 2 }, { 5, 8 } }),
+                        new SparseMatrix(new int[][] { { 53, 62 }, { 69, 80 } })));
+    }
+
+    /**
+     * testMultiply test the cases for multiplication
+     * @param SparseMatrix a object of SparseMatrix to be multiplied
+     * @param SparseMatrix b object of SparseMatrix to be multiplied
+     * @param SparseMatrix result the output of multiplication
+     * @throws Exception if error is present
+     */
+    @ParameterizedTest
+    @MethodSource("multiply")
+    void testMultiply(SparseMatrix a, SparseMatrix b, SparseMatrix result) throws Exception {
+        System.out.println(a.multiplication(b));
+        System.out.println(result);
+        assertTrue(a.multiplication(b).equals(result.mp));
+
+    }
+
+    /**
+     * addition returns and stream of Arguments to be checked as a input
+     * 
+     * @return Stream<Arguments> or the input to be checked
+     */
+    private static Stream<Arguments> addition() {
+        return Stream.of(
+                Arguments.of(new SparseMatrix(new int[][] { { 3, 7 }, { 4, 9 } }),
+                        new SparseMatrix(new int[][] { { 6, 2 }, { 5, 8 } }),
+                        new SparseMatrix(new int[][] { { 9, 9 }, { 9, 17 } })));
+    }
+
+    /**
+     * testaddition test the cases for addition
+     * @param SparseMatrix a object of SparseMatrix to be Added
+     * @param SparseMatrix b object of SparseMatrix to be Added
+     * @param SparseMatrix result the output of Addition
+     * @throws Exception if error is present
+     */
+    @ParameterizedTest
+    @MethodSource("addition")
+    void testadditio(SparseMatrix a, SparseMatrix b, SparseMatrix result) throws Exception {
+        assertTrue(a.addition(b).equals(result.mp));
+
+    }
+
+     /**
+     * transPose returns and stream of Arguments to be checked as a input
+     * 
+     * @return Stream<Arguments> or the input to be checked
+     */
+    private static Stream<Arguments> transPose() {
+        return Stream.of(
+                Arguments.of(new SparseMatrix(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }),
+                        new SparseMatrix(new int[][] { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } })));
+    }
+
+    /**
+     * testTranspose test the cases for Transposing
+     * @param SparseMatrix a object of SparseMatrix to be Transposed
+     * @param SparseMatrix result the output of Transposing
+     * @throws Exception if error is present
+     */
+    @ParameterizedTest
+    @MethodSource("transPose")
+    void testTranspose(SparseMatrix a, SparseMatrix result) throws Exception {
+        assertFalse(a.transPose().equals(result));
+
+    }
+
+    /**
+     * Symmetric returns and stream of Arguments to be checked as a input
+     * 
+     * @return Stream<Arguments> or the input to be checked
+     */
+    private static Stream<Arguments> Symmetric() {
+        return Stream.of(
+                Arguments.of(new SparseMatrix(new int[][] { { 1, 3, 5 }, { 3, 2, 4 }, { 5, 4, 2 } })));
+    }
+
+    /**
+     * testSymmetric test the cases for Symmetric
+     * @param SparseMatrix a object of SparseMatrix to be checked is Symmetric or
+     * @throws Exception if error is present
+     */
+    @ParameterizedTest
+    @MethodSource("Symmetric")
+    void testSymmetric(SparseMatrix a) throws Exception {
+        assertTrue(a.symmetrical());
+
+    }
 
 }
