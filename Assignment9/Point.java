7bd6ff9dc682b588602e60777277c7e619e9d861
Kanishk Pamecha <kanishk.pamecha@metacube.com>
2025-03-19 16:14:22 +0530

    Upadte 9

--- Assignment9/Point.java
+++ Assignment9/Point.java
@@ -7,14 +7,23 @@ public class Point {
         this.y = j;
     }
 
+    /**
+     * getter for the x point
+     */
     public int getX() {
         return x;
     }
-
+    /**
+     * getter for the y point
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * distance return the distance from the origin to the point 
+     * @return double distance to the user
+     */
     public double distance() {
         return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
