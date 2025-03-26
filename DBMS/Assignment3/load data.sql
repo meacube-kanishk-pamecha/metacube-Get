6f781e3540a9c559bdcc34ebcf2e816d10a2c853
Kanishk Pamecha <kanishk.pamecha@metacube.com>
2025-03-26 22:59:01 +0530

    DBMS

--- DBMS/Assignment3/load data.sql
+++ DBMS/Assignment3/load data.sql
@@ -1,14 +1,26 @@
 create DATABASE School;
 USE School;
 CREATE TABLE student (
-id INT NOT NULL,
-first_name VARCHAR(50) NOT NULL,
-last_name VARCHAR(50) NOT NULL);
+id INT ,
+first_name VARCHAR(50) ,
+last_name VARCHAR(50) );
 
-show variables LIKE 'secure_file_priv';
--- secure-file-priv=""
+
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/data.txt' 
 INTO TABLE student 
 FIELDS TERMINATED BY ','  
 LINES TERMINATED BY '\n';   
 select * from student;
+
+ALTER TABLE Student
+ADD CONSTRAINT PK_Person PRIMARY KEY (id);
+ALTER TABLE student
+modify COLUMN id int NOT NULL;
+
+-- ALTER TABLE 
+-- ADD FOREIGN KEY () REFERENCES xyz();
+
+ALTER TABLE student
+ADD CONSTRAINT UC_Person UNIQUE (ID); 
+
+ALTER TABLE student AUTO_INCREMENT=100;
