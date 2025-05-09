1e62094fb5ebfbc8ee4fae789a09269822454a87
Kanishk Pamecha <kanishk.pamecha@metacube.com>
2025-05-09 11:04:49 +0530

    assignment

---
+++ UI/Assignment10/assignment/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskManager from './Components/TaskManager';
+
+function App() {
+  return (
+  <div>
+    <TaskManager/>
+  </div>
+  );
+}
+
+export default App;
